bcc2442675028afbbe3e11c7de327cfb0afab855
Gautam <singhalgautam22@gmail.com>
2024-12-03 02:11:17 +0530

    add requirements and basic code

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operation import add, sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    assert add (-1,-1)==0
+
+def test_sub():
+    assert sub(2,3)==-1
+    assert sub(3,3)==0
+    assert sub(-2,3)==-5
